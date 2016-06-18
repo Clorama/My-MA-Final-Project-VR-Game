76a78f0082c36c3776915694375da6b507fd80be
clorama dorvilias <cloramadorvilias@pc52.home>
2016-06-18 14:23:02 +0200

    adding all files for the VR Game

---
+++ Assets/InstantVR/Editor/IVR_Rift_Editor.cs
@@ -0,0 +1,72 @@
+﻿/* InstantVR Oculus Rift extension editor
+ * author: Pascal Serrarens
+ * email: support@passervr.com
+ * version: 3.1.0
+ * date: September 26, 2015
+ * 
+ * - Check for VirtualRealitySupported added
+ */
+
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[CustomEditor(typeof(IVR_Rift))] 
+public class IVR_Rift_Editor : IVR_Extension_Editor {
+
+	private InstantVR ivr;
+	private IVR_Rift ivrRift;
+
+	private IVR_RiftHead riftHead;
+
+#if !UNITY_ANDROID
+#if !(UNITY_4_5 || UNITY_4_6 || UNITY_5_0)
+    public override void OnInspectorGUI() {
+        if (PlayerSettings.virtualRealitySupported == false)
+            EditorGUILayout.HelpBox("VirtualRealitySupported needs to be enabled in Player Settings for Rift support", MessageType.Warning, true);
+
+        base.OnInspectorGUI();
+    }
+#endif
+#endif
+
+	void OnDestroy() {
+		if (ivrRift == null && ivr != null) {
+			riftHead = ivr.headTarget.GetComponent<IVR_RiftHead>();
+			if (riftHead != null)
+				DestroyImmediate(riftHead, true);
+		}
+	}
+	
+	void OnEnable() {
+
+		ivrRift = (IVR_Rift) target;
+		ivr = ivrRift.GetComponent<InstantVR>();
+
+		if (ivr != null) {
+			riftHead = ivr.headTarget.GetComponent<IVR_RiftHead>();
+			if (riftHead == null) {
+				riftHead = ivr.headTarget.gameObject.AddComponent<IVR_RiftHead>();
+				riftHead.extension = ivrRift;
+			}
+
+			IVR_Extension[] extensions = ivr.GetComponents<IVR_Extension>();
+			if (ivrRift.priority == -1)
+				ivrRift.priority = extensions.Length - 1;
+			for (int i = 0; i < extensions.Length; i++) {
+				if (ivrRift == extensions[i]) {
+					while (i < ivrRift.priority) {
+						MoveUp(riftHead);
+						ivrRift.priority--;
+						//Debug.Log ("Rift Move up to : " + i + " now: " + ivrRift.priority);
+					}
+					while (i > ivrRift.priority) {
+						MoveDown(riftHead);
+						ivrRift.priority++;
+						//Debug.Log ("Rift Move down to : " + i + " now: " + ivrRift.priority);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/InstantVR/Extensions/Cardboard/IVR_CardboardHead.cs
@@ -0,0 +1,102 @@
+﻿/* InstantVR Cardboard head controller
+ * author: Pascal Serrarens
+ * email: support@passervr.com
+ * version: 3.1.0
+ * date: August 19, 2015
+ * 
+ * - Bugfixes
+ */
+
+using UnityEngine;
+#if !UNITY_4_5
+using UnityEngine.EventSystems;
+#endif
+using System.Collections;
+
+public class IVR_CardboardHead : IVR_Controller {
+#if !UNITY_4_5
+    [Tooltip("Adds a PhysicsRaycaster for gaze selection")]
+    public bool raycaster = false;
+#endif
+
+    private Cardboard cardboard;
+
+    [HideInInspector]
+    private Vector3 cardboardStartPoint;
+    [HideInInspector]
+    private Vector3 baseStartPoint;
+    [HideInInspector]
+    private Quaternion cardboardStartRotation;
+    [HideInInspector]
+    private float baseStartAngle;
+
+    public override void StartController(InstantVR ivr) {
+#if UNITY_ANDROID
+        Transform headcam = this.transform.FindChild("Headcam");
+        if (headcam != null) {
+            headcam.gameObject.SetActive(false);
+
+            Cardboard cardboardPrefab = Resources.Load<Cardboard>("CardboardPrefab");
+            cardboard = (Cardboard)Instantiate(cardboardPrefab, headcam.position, headcam.rotation);
+        }
+
+        if (cardboard == null)
+            Debug.LogError("Could not instantiate Cardboard. CardboardCameraRig is missing?");
+        else {
+            cardboard.transform.parent = ivr.transform;
+
+            cardboardStartPoint = cardboard.transform.position;
+            baseStartPoint = ivr.BasePosition;
+            cardboardStartRotation = cardboard.transform.rotation;
+            baseStartAngle = ivr.BaseRotation.eulerAngles.y;
+
+            present = true;
+            if (!present) {
+                cardboard.gameObject.SetActive(false);
+                headcam.gameObject.SetActive(true);
+            }  else
+                headcam.gameObject.SetActive(false);
+#if !UNITY_4_5
+            if (raycaster) {
+                GameObject mainCamera = cardboard.GetComponentInChildren<StereoController>().gameObject;
+                mainCamera.AddComponent<PhysicsRaycaster>();
+            }
+#endif
+        }
+        base.StartController(ivr);
+        startRotation = Quaternion.Inverse(ivr.BaseRotation);
+#endif
+    }
+
+    public override void UpdateController() {
+#if UNITY_ANDROID
+        if (present && this.enabled)
+            UpdateCardboard();
+        else
+            tracking = false;
+#endif
+    }
+
+#if UNITY_ANDROID
+    private void UpdateCardboard() {
+        if (cardboard != null) {
+            Vector3 baseDelta = ivr.BasePosition - baseStartPoint;
+            cardboard.transform.position = cardboardStartPoint + baseDelta;
+
+            float baseAngleDelta = ivr.BaseRotation.eulerAngles.y - baseStartAngle;
+            cardboard.transform.rotation = cardboardStartRotation * Quaternion.AngleAxis(baseAngleDelta, Vector3.up);
+
+            if (cardboard.HeadPose != null) {
+                this.rotation = cardboard.HeadPose.Orientation * Quaternion.AngleAxis(-baseAngleDelta, Vector3.up); ;
+
+                if (!tracking) {
+                    Calibrate(false);
+                    tracking = true;
+                }
+
+                base.UpdateController();
+            }
+        }
+    }
+#endif
+}
